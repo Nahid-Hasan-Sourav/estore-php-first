896b18aac64f46f32236d3f2e6bd92a8fd8667a7
Nahid-Hasan_Sourav <nahidhasansourav05@gmail.com>
2023-02-16 23:50:18 +0600

    added a collaborate section

--- pages/home.php
+++ pages/home.php
@@ -1,6 +1,8 @@
 <?php include 'header.php' ?>
 
+    <!--    BANNER PART START HERE-->
 <section>
+
     <div class="container">
         <div class="row">
             <div class="col-md-6 justify-content-center d-flex flex-column">
@@ -12,7 +14,7 @@
                     At Bachira, you can shop for all your favorite beauty brands, clothes,<br>
                     household products and more at a single place.
                 </p>
-                <button type="button" class="btn btn-success mt-3 d-block"
+                <button type="button" class="btn btn-success mt-3 d-block text-uppercase"
                 style="width: 30%;"
                 >Success</button>
             </div>
@@ -25,7 +27,30 @@
             </div>
         </div>
     </div>
-</section>
 
+</section>
+    <!--    BANNER PART END HERE-->
+
+    <!--    COLLABORATE WITH.. START HERE-->
+<section>
+    <div class="container my-4">
+       <div>
+           <h4 class="text-center fw-bold">
+               We Collaborate With 250++ Leading Top<br>
+               E Commerce and Brands
+           </h4>
+       </div>
+        <div class="d-flex align-content-center justify-content-center py-4">
+            <marquee direction="">
+            <img class="mx-3" style="width: 202px; height:33px;" src="asset/img/tokopedia.png"/>
+            <img class="mx-3" style="width: 202px; height:33px;" src="asset/img/Balenciaga%20svg.png"/>
+            <img class="mx-3" style="width: 202px; height:33px;" src="asset/img/adidas%20svg.png"/>
+            <img class="mx-3" style="width: 202px; height:33px;" src="asset/img/gucci%20svg.png"/>
+            <img class="mx-3" style="width: 202px; height:33px;" src="asset/img/Balenciaga%20svg.png"/>
+            </marquee>
+        </div>
+    </div>
+</section>
+    <!--    COLLABORATE WITH.. END HERE-->
 
 <?php include 'footer.php' ?>
